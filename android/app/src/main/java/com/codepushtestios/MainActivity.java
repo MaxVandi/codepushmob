c67a580a55bbf2ce772a28fd88d8c07d13312eb2
da <da>
2019-07-03 12:02:42 +0200

    Alleeeeeez

--- android/app/src/main/java/com/codepushtestios/MainActivity.java
+++ android/app/src/main/java/com/codepushtestios/MainActivity.java
@@ -3,7 +3,11 @@ package com.codepushtestios;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-
+    new CodePush(
+    "RO4_VmIqPIVUOKrnUU7RwBacdNYG7cefd3de-91e3-4da9-974e-9f748d672837",
+    getApplicationContext(),
+    BuildConfig.DEBUG,
+    R.string.CodePushPublicKey)
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
